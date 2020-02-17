e80852f33396b55844facc3690465e2c7d245408
Mitch Lum <mklum88@gmail.com>
2020-02-17 15:41:31 -0800

    revert to boilerplate code

---
+++ src/Header.js
@@ -0,0 +1,80 @@
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import Header from "./components/Header/Header.js";
+import Footer from "./components/Footer/Footer.js";
+import GridContainer from "./components/Grid/GridContainer.js";
+import GridItem from "./components/Grid/GridItem.js";
+import Button from "./components/CustomButtons/Button.js";
+import HeaderLinks from "./components/Header/HeaderLinks.js";
+import Parallax from "./components/Parallax/Parallax.js";
+
+const dashboardRoutes = [];
+const useStyles = makeStyles(theme => ({
+  subtitle: {
+    fontSize: "1.313rem",
+    maxWidth: "500px",
+    margin: "10px auto 0"
+  },
+  main: {
+    background: "#FFFFFF",
+    position: "relative",
+    zIndex: "3"
+  },
+  mainRaised: {
+    margin: "-60px 30px 0px",
+    borderRadius: "6px",
+    boxShadow:
+      "0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)"
+  }
+}));
+
+export default function LandingPage(props) {
+  const classes = useStyles();
+  const { ...rest } = props;
+  return (
+    <div>
+      <Header
+        color="transparent"
+        routes={dashboardRoutes}
+        brand="Material Kit React"
+        rightLinks={<HeaderLinks />}
+        fixed
+        changeColorOnScroll={{
+          height: 400,
+          color: "white"
+        }}
+        {...rest}
+      />
+      <Parallax filter image={require("./assets/img/landing-bg.jpg")}>
+        <div className={classes.container}>
+          <GridContainer>
+            <GridItem xs={12} sm={12} md={6}>
+              <h1 className={classes.title}>Your Story Starts With Us.</h1>
+              <h4>
+                Every landing page needs a small description after the big bold
+                title, that{"'"}s why we added this text here. Add here all the
+                information that can make you or your product create the first
+                impression.
+              </h4>
+              <br />
+              <Button
+                color="danger"
+                size="lg"
+                href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ref=creativetim"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fas fa-play" />
+                Watch video
+              </Button>
+            </GridItem>
+          </GridContainer>
+        </div>
+      </Parallax>
+      <Footer />
+    </div>
+  );
+}
