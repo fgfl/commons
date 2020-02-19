58311dbc46b39ac0908a567cc0ca78d156d50f5d
fgfl <frederick.gfl@gmail.com>
2020-02-18 17:34:20 -0800

    Move material ui form to Mitch's login page

---
+++ src/__tests__/Login.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+
+import Login from '../components/registrations/Login';
+import { MemoryRouter, BrowserRouter as Router } from 'react-router';
+
+afterEach(cleanup);
+
+it('renders the login form', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <Login></Login>
+    </MemoryRouter>
+  );
+  expect(getByText('Email Address*')).toBeInTheDocument();
+});
