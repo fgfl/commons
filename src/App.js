533381bbc697c6f9ede70986d88cbe4cc67ef1ca
Mitch Lum <mitchlum@Mitchs-MacBook-Pro.local>
2020-02-18 18:06:42 -0800

    set up router and nav links

--- src/App.js
+++ src/App.js
@@ -1,87 +1,23 @@
-import React, { Component } from "react";
-import axios from "axios";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import NavBar from "./components/Nav";
 import Home from "./components/Home";
+import Watch from "./components/Watch";
 import Login from "./components/registrations/Login";
 import Signup from "./components/registrations/Signup";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoggedIn: false,
-      user: {}
-    };
-  }
-  componentDidMount() {
-    this.loginStatus();
-  }
-  loginStatus = () => {
-    axios
-      .get("http://localhost:3001/logged_in", { withCredentials: true })
-      .then(response => {
-        if (response.data.logged_in) {
-          this.handleLogin(response);
-        } else {
-          this.handleLogout();
-        }
-      })
-      .catch(error => console.log("api errors:", error));
-  };
-  handleLogin = data => {
-    this.setState({
-      isLoggedIn: true,
-      user: data.user
-    });
-  };
-  handleLogout = () => {
-    this.setState({
-      isLoggedIn: false,
-      user: {}
-    });
-  };
-  render() {
-    return (
-      <div>
-        <BrowserRouter>
-          <Switch>
-            <Route
-              exact
-              path="/"
-              render={props => (
-                <Home
-                  {...props}
-                  handleLogout={this.handleLogout}
-                  loggedInStatus={this.state.isLoggedIn}
-                />
-              )}
-            />
-            <Route
-              exact
-              path="/login"
-              render={props => (
-                <Login
-                  {...props}
-                  handleLogin={this.handleLogin}
-                  loggedInStatus={this.state.isLoggedIn}
-                />
-              )}
-            />
-            <Route
-              exact
-              path="/signup"
-              render={props => (
-                <Signup
-                  {...props}
-                  handleLogin={this.handleLogin}
-                  loggedInStatus={this.state.isLoggedIn}
-                />
-              )}
-            />
-          </Switch>
-        </BrowserRouter>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <Router>
+        <NavBar />
+        <Route path="/" exact component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/Watch" component={Watch} />
+      </Router>
+    </div>
+  );
 }
+
 export default App;
