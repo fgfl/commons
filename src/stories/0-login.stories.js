d413e624a7241dbdc261c05724d1c2ece7c4ec8f
fgfl <frederick.gfl@gmail.com>
2020-02-21 14:51:37 -0800

    Add home button to sucess form

--- src/stories/0-login.stories.js
+++ src/stories/0-login.stories.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
-import LoginForm from '../components/LoginForm';
+import LoginForm from '../components/registrations/Login';
 
 storiesOf('LoginForm', module)
   .addParameters({
