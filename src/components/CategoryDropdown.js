fb78fc9b1daa93d74f2e69523c671feb44ba3ac0
Pascal van Leeuwen <commoddity@users.noreply.github.com>
2020-02-20 17:01:45 -0800

    Added categories as props to the react front end from the Rails back end.

--- src/components/CategoryDropdown.js
+++ src/components/CategoryDropdown.js
@@ -1,83 +1,56 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import TextField from "@material-ui/core/TextField";
-import MenuItem from "@material-ui/core/MenuItem";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
 
-const categories = [
-  {
-    value: "All Categories",
-    label: "All Categories"
-  },
-  {
-    value: "Transport",
-    label: "Transport"
-  },
-  {
-    value: "Indigenous Rights",
-    label: "Indigenous Rights"
-  },
-  {
-    value: "Agriculture",
-    label: "Agriculture"
-  },
-  {
-    value: "Finance",
-    label: "Finance"
-  },
-  {
-    value: "Transport",
-    label: "Transport"
-  },
-  {
-    value: "Indigenous Rights",
-    label: "Indigenous Rights"
-  },
-  {
-    value: "Agriculture",
-    label: "Agriculture"
-  }
-];
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    "& .MuiTextField-root": {
-      width: "100%",
-      textAlign: "left",
-      marginBottom: "16px",
-      marginTop: "16px"
-    }
-  },
-  text: {
-    margin: "0 auto"
-  }
+const useStyles = makeStyles((theme) => ({
+	root: {
+		'& .MuiTextField-root': {
+			width: '100%',
+			textAlign: 'left',
+			marginBottom: '16px',
+			marginTop: '16px'
+		}
+	},
+	text: {
+		margin: '0 auto'
+	}
 }));
 
-export default function CategoryDropdown() {
-  const classes = useStyles();
-  const [category, setCategory] = React.useState("All Categories");
+export default function CategoryDropdown(props) {
+	const classes = useStyles();
+	const [category, setCategory] = React.useState('All Categories');
 
-  const handleChange = event => {
-    setCategory(event.target.value);
-  };
+	const categories = [];
+	props.categories.forEach((c) => {
+		categories.push({
+			value: c.name,
+			label: c.name
+		});
+	});
 
-  return (
-    <form className={classes.root} noValidate autoComplete="off">
-      <div>
-        <TextField
-          id="standard-select-currency"
-          select
-          label="Select"
-          value={category}
-          onChange={handleChange}
-          helperText="Please select a category"
-        >
-          {categories.map(option => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
-      </div>
-    </form>
-  );
+	const handleChange = (event) => {
+		setCategory(event.target.value);
+	};
+
+	return (
+		<form className={classes.root} noValidate autoComplete='off'>
+			<div>
+				<TextField
+					id='standard-select-currency'
+					select
+					label='Select'
+					value={category}
+					onChange={handleChange}
+					helperText='Please select a category'
+				>
+					{categories.map((option) => (
+						<MenuItem key={option.value} value={option.value}>
+							{option.label}
+						</MenuItem>
+					))}
+				</TextField>
+			</div>
+		</form>
+	);
 }
