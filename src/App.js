07adbae1ef3206cd06bb9729fe8b2e571f39eaaa
Mitch Lum <mitchlum@Mitchs-MacBook-Pro.local>
2020-02-20 18:10:43 -0800

    fixed signup form component location

--- src/App.js
+++ src/App.js
@@ -1,115 +1,115 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import axios from 'axios';
-import NavBar from './components/Nav';
-import Home from './components/Home';
-import Watch from './components/Watch';
-import Login from './components/registrations/Login';
-import Signup from './components/registrations/Signup';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import axios from "axios";
+import NavBar from "./components/Nav";
+import Home from "./components/Home";
+import Watch from "./components/Watch";
+import Login from "./components/registrations/Login";
+import Signup from "./components/registrations/Signup/Signup";
 
 const App = () => {
-	const [user, setUser] = useState({});
-	const [loggedIn, setLoggedIn] = useState(false);
-	const [bills, setBills] = useState([]);
-	const [categories, setCategories] = useState([]);
-	const [events, setEvents] = useState([]);
+  const [user, setUser] = useState({});
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [bills, setBills] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [events, setEvents] = useState([]);
 
-	useEffect(() => {
-		loginStatus();
-		fetchBills();
-	}, []);
+  useEffect(() => {
+    loginStatus();
+    fetchBills();
+  }, []);
 
-	const loginStatus = () => {
-		axios
-			.get('http://localhost:3001/logged_in', { withCredentials: true })
-			.then((response) => {
-				if (response.data.logged_in) {
-					handleLogin(response);
-				} else {
-					handleLogout();
-				}
-			})
-			.catch((error) => console.log('api errors:', error));
-	};
+  const loginStatus = () => {
+    axios
+      .get("http://localhost:3001/logged_in", { withCredentials: true })
+      .then(response => {
+        if (response.data.logged_in) {
+          handleLogin(response);
+        } else {
+          handleLogout();
+        }
+      })
+      .catch(error => console.log("api errors:", error));
+  };
 
-	const fetchBills = () => {
-		axios
-			.get('http://localhost:3001/bills')
-			.then((response) => {
-				setBills(response.data.bills);
-				setCategories(response.data.categories);
-			})
-			.catch((error) => console.log('api errors:', error));
-	};
+  const fetchBills = () => {
+    axios
+      .get("http://localhost:3001/bills")
+      .then(response => {
+        setBills(response.data.bills);
+        setCategories(response.data.categories);
+      })
+      .catch(error => console.log("api errors:", error));
+  };
 
-	const handleLogin = (data) => {
-		setUser(data.user);
-		console.log('logged in');
-		setLoggedIn(true);
-	};
+  const handleLogin = data => {
+    setUser(data.user);
+    console.log("logged in");
+    setLoggedIn(true);
+  };
 
-	const handleLogout = () => {
-		setUser({});
-		setLoggedIn(false);
-	};
+  const handleLogout = () => {
+    setUser({});
+    setLoggedIn(false);
+  };
 
-	return (
-		<div>
-			<Router>
-				<NavBar user={user} handleLogout={handleLogout} loggedIn={loggedIn} />
-				<Switch>
-					<Route
-						exact
-						path='/'
-						render={(props) => (
-							<Home
-								{...props}
-								bills={bills}
-								categories={categories}
-								handleLogout={handleLogout}
-								loggedInStatus={loggedIn}
-							/>
-						)}
-					/>
-					<Route
-						exact
-						path='/login'
-						render={(props) => (
-							<Login
-								{...props}
-								handleLogin={handleLogin}
-								loggedInStatus={loggedIn}
-							/>
-						)}
-					/>
-					<Route
-						exact
-						path='/signup'
-						render={(props) => (
-							<Signup
-								{...props}
-								handleLogin={handleLogin}
-								loggedInStatus={loggedIn}
-							/>
-						)}
-					/>
-					<Route
-						exact
-						path='/logout'
-						render={(props) => (
-							<Home
-								{...props}
-								handleLogin={handleLogout}
-								loggedInStatus={loggedIn}
-							/>
-						)}
-					/>
-					<Route path='/Watch' component={Watch} />
-					{/* <Route path="/user/:id" component={Profile} /> */}
-				</Switch>
-			</Router>
-		</div>
-	);
+  return (
+    <div>
+      <Router>
+        <NavBar user={user} handleLogout={handleLogout} loggedIn={loggedIn} />
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={props => (
+              <Home
+                {...props}
+                bills={bills}
+                categories={categories}
+                handleLogout={handleLogout}
+                loggedInStatus={loggedIn}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/login"
+            render={props => (
+              <Login
+                {...props}
+                handleLogin={handleLogin}
+                loggedInStatus={loggedIn}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/signup"
+            render={props => (
+              <Signup
+                {...props}
+                handleLogin={handleLogin}
+                loggedInStatus={loggedIn}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/logout"
+            render={props => (
+              <Home
+                {...props}
+                handleLogin={handleLogout}
+                loggedInStatus={loggedIn}
+              />
+            )}
+          />
+          <Route path="/Watch" component={Watch} />
+          {/* <Route path="/user/:id" component={Profile} /> */}
+        </Switch>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
--- src/components/CategoryDropdown.js
+++ src/components/CategoryDropdown.js
@@ -1,56 +1,57 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import MenuItem from '@material-ui/core/MenuItem';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import MenuItem from "@material-ui/core/MenuItem";
 
-const useStyles = makeStyles((theme) => ({
-	root: {
-		'& .MuiTextField-root': {
-			width: '100%',
-			textAlign: 'left',
-			marginBottom: '16px',
-			marginTop: '16px'
-		}
-	},
-	text: {
-		margin: '0 auto'
-	}
+const useStyles = makeStyles(theme => ({
+  root: {
+    "& .MuiTextField-root": {
+      width: "100%",
+      textAlign: "left",
+      marginBottom: "16px",
+      marginTop: "16px"
+    }
+  },
+  text: {
+    margin: "0 auto"
+  }
 }));
 
 export default function CategoryDropdown(props) {
-	const classes = useStyles();
-	const [category, setCategory] = React.useState('All Categories');
+  const classes = useStyles();
+  const [category, setCategory] = React.useState("All Categories");
 
-	const categories = [];
-	props.categories.forEach((c) => {
-		categories.push({
-			value: c.name,
-			label: c.name
-		});
-	});
+  const categories = [];
 
-	const handleChange = (event) => {
-		setCategory(event.target.value);
-	};
+  props.categories.forEach(c => {
+    categories.push({
+      value: c.name,
+      label: c.name
+    });
+  });
 
-	return (
-		<form className={classes.root} noValidate autoComplete='off'>
-			<div>
-				<TextField
-					id='standard-select-currency'
-					select
-					label='Select'
-					value={category}
-					onChange={handleChange}
-					helperText='Please select a category'
-				>
-					{categories.map((option) => (
-						<MenuItem key={option.value} value={option.value}>
-							{option.label}
-						</MenuItem>
-					))}
-				</TextField>
-			</div>
-		</form>
-	);
+  const handleChange = event => {
+    setCategory(event.target.value);
+  };
+
+  return (
+    <form className={classes.root} noValidate autoComplete="off">
+      <div>
+        <TextField
+          id="standard-select-currency"
+          select
+          label="Select"
+          value={category}
+          onChange={handleChange}
+          helperText="Please select a category"
+        >
+          {categories.map(option => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+      </div>
+    </form>
+  );
 }
