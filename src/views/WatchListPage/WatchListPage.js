3307da996b7c49d62ed11e9126651c60babc2dfc
Mitch Lum <mklum88@gmail.com>
2020-02-22 01:23:52 -0800

    added profile page and removed unused imports

--- src/views/WatchListPage/WatchListPage.js
+++ src/views/WatchListPage/WatchListPage.js
@@ -1,70 +1,77 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 // nodejs library that concatenates classes
 import classNames from "classnames";
+// react components for routing our app without refresh
+import { Link } from "react-router-dom";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-
 // @material-ui/icons
-
 // core components
 import Header from "components/Header/Header.js";
 import Footer from "components/Footer/Footer.js";
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
-import Button from "components/CustomButtons/Button.js";
-import HeaderLinks from "components/Header/HeaderLinks.js";
 import Parallax from "components/Parallax/Parallax.js";
+// sections for this page
+import HeaderLinks from "components/Header/HeaderLinks.js";
 
-import styles from "assets/jss/material-kit-react/views/landingPage.js";
-
-const dashboardRoutes = [];
+import styles from "assets/jss/material-kit-react/views/components.js";
+import CategoryDropdown from "../HomePage/CategoryDropdown";
+import Bill from "../HomePage/Bill";
 
 const useStyles = makeStyles(styles);
 
-export default function LandingPage(props) {
+export default function WatchListPage(props) {
   const classes = useStyles();
   const { ...rest } = props;
+
+  const [childCategory, setChildCategory] = useState(0);
+
+  useEffect(() => console.log("Category ID: ", childCategory), [childCategory]);
+
+  const bills = props.bills.filter(bill => {
+    return childCategory === 0 ? bill : bill.categories.includes(childCategory);
+  });
+
+  const billCards = bills.map(bill => {
+    return <Bill key={bill.id} bill={bill} />;
+  });
+
   return (
     <div>
       <Header
-        color="transparent"
-        routes={dashboardRoutes}
-        brand="Material Kit React"
+        brand="Commons"
         rightLinks={<HeaderLinks />}
         fixed
+        color="transparent"
         changeColorOnScroll={{
           height: 400,
           color: "white"
         }}
         {...rest}
       />
-      <Parallax filter image={require("assets/img/landing-bg.jpg")}>
+      <Parallax image={require("assets/img/bg4.jpg")}>
         <div className={classes.container}>
           <GridContainer>
-            <GridItem xs={12} sm={12} md={6}>
-              <h1 className={classes.title}>Your Story Starts With Us.</h1>
-              <h4>
-                Every landing page needs a small description after the big bold
-                title, that{"'"}s why we added this text here. Add here all the
-                information that can make you or your product create the first
-                impression.
-              </h4>
-              <br />
-              <Button
-                color="danger"
-                size="lg"
-                href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ref=creativetim"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="fas fa-play" />
-                Watch video
-              </Button>
+            <GridItem>
+              <div className={classes.brand}>
+                <h1 className={classes.title}>Welcome to Commons</h1>
+                <h3 className={classes.subtitle}>
+                  Get informed. Stay engaged.
+                </h3>
+              </div>
             </GridItem>
           </GridContainer>
         </div>
       </Parallax>
-      <div className={classNames(classes.main, classes.mainRaised)}></div>
+
+      <div className={classNames(classes.main, classes.mainRaised)}>
+        <CategoryDropdown
+          categories={props.categories}
+          passCategory={setChildCategory}
+        />
+        {billCards}
+      </div>
       <Footer />
     </div>
   );
