255d98ca5c2d6725a25d13407c69431fcada0f46
Mitch Lum <mklum88@gmail.com>
2020-02-23 00:57:26 -0800

    fixed watch list page props

--- src/views/WatchListPage/WatchListPage.js
+++ src/views/WatchListPage/WatchListPage.js
@@ -17,64 +17,73 @@ import HeaderLinks from 'components/Header/HeaderLinks.js';
 
 import styles from 'assets/jss/material-kit-react/views/components.js';
 import CategoryDropdown from '../HomePage/CategoryDropdown';
-import Bill from '../HomePage/Bills';
+import Bills from '../HomePage/Bills';
 import image from 'assets/img/bg2.jpg';
 
 const useStyles = makeStyles(styles);
 
 export default function WatchListPage(props) {
-	const classes = useStyles();
-	const { ...rest } = props;
+  const [clicked, setClicked] = useState({});
+  const classes = useStyles();
+  const { ...rest } = props;
 
-	const [childCategory, setChildCategory] = useState(0);
+  const [childCategory, setChildCategory] = useState(0);
 
-	useEffect(() => console.log('Category ID: ', childCategory), [childCategory]);
+  useEffect(() => console.log('Category ID: ', childCategory), [childCategory]);
 
-	const bills = props.bills.filter((bill) => {
-		return childCategory === 0 ? bill : bill.categories.includes(childCategory);
-	});
+  const setThisOneClicked = (key) => {
+    setClicked((prev) => {
+      let state = { ...prev };
+      if (state[key]) {
+        delete state[key];
+      } else {
+        state[key] = true;
+      }
+      return state;
+    });
+  };
 
-	const billCards = bills.map((bill) => {
-		return <Bill key={bill.id} bill={bill} />;
-	});
+  return (
+    <div>
+      <Header
+        brand='Commons'
+        rightLinks={<HeaderLinks />}
+        fixed
+        color='transparent'
+        changeColorOnScroll={{
+          height: 400,
+          color: 'white'
+        }}
+        {...rest}
+      />
+      <div
+        className={classes.pageHeader}
+        style={{
+          backgroundImage: 'url(' + image + ')',
+          backgroundSize: 'cover',
+          backgroundPosition: 'top center'
+        }}
+      ></div>
+      <Parallax image={require('assets/img/bg2.jpg')}>
+        <div className={classes.container}>
+          <GridContainer>
+            <GridItem></GridItem>
+          </GridContainer>
+        </div>
+      </Parallax>
 
-	return (
-		<div>
-			<Header
-				brand='Commons'
-				rightLinks={<HeaderLinks />}
-				fixed
-				color='transparent'
-				changeColorOnScroll={{
-					height: 400,
-					color: 'white'
-				}}
-				{...rest}
-			/>
-			<div
-				className={classes.pageHeader}
-				style={{
-					backgroundImage: 'url(' + image + ')',
-					backgroundSize: 'cover',
-					backgroundPosition: 'top center'
-				}}
-			></div>
-			<Parallax image={require('assets/img/bg2.jpg')}>
-				<div className={classes.container}>
-					<GridContainer>
-						<GridItem></GridItem>
-					</GridContainer>
-				</div>
-			</Parallax>
-
-			<div className={classNames(classes.main, classes.mainRaised)}>
-				<CategoryDropdown
-					categories={props.categories}
-					passCategory={setChildCategory}
-				/>
-				{billCards}
-			</div>
-			<Footer />
-		</div>
-	);
+      <div className={classNames(classes.main, classes.mainRaised)}>
+        <CategoryDropdown
+          categories={props.categories}
+          passCategory={setChildCategory}
+        />
+        <Bills
+          bills={props.bills}
+          clicked={clicked}
+          setThisOneClicked={setThisOneClicked}
+        />
+      </div>
+      <Footer />
+    </div>
+  );
 }
