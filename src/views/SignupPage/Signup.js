91bbea8c61eefe24e6bc61fa744c4cc4de91493b
Pascal van Leeuwen <commoddity@users.noreply.github.com>
2020-02-23 13:53:24 -0800

    Added signup validation erro messages. Now need to actually validate.

--- src/views/SignupPage/Signup.js
+++ src/views/SignupPage/Signup.js
@@ -1,22 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import PersonIcon from '@material-ui/icons/Person';
 import { Typography, TextField, Button, Avatar } from '@material-ui/core';
 
 const Signup = (props) => {
-  let data = {
+  const [state, setState] = useState({
     name: '',
     username: '',
     email: '',
     password: '',
     password_confirmation: '',
-    postal_code: ''
+    postal_code: '',
+    errors: {
+      name: '',
+      username: '',
+      email: '',
+      password: '',
+      password_confirmation: ''
+    }
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event;
+    let errors = state.errors;
+
+    switch (name) {
+      case 'name':
+        errors.name = value.length < 1 ? 'Name must be present!' : '';
+        break;
+      case 'username':
+        errors.name =
+          value.length < 4 || !validUsernameRegex.test(value)
+            ? 'Username must be 4 characters long and only contain valid characters!'
+            : '';
+        break;
+      case 'email':
+        errors.email = validEmailRegex.test(value) ? '' : 'Email is not valid!';
+        break;
+      case 'password':
+        errors.password =
+          value.length < 8 ? 'Password must be 8 characters long!' : '';
+        break;
+      case 'password confirmation':
+        errors.password =
+          value.length !== state.password
+            ? 'Password and password confirmation must match!'
+            : '';
+        break;
+      default:
+        break;
+    }
+    setState((prevState) => ({
+      ...prevState,
+      [name]: value,
+      errors
+    }));
+    console.log(errors);
+    console.log('STATE', state.errors);
   };
+
+  const validUsernameRegex = RegExp(/^([a-zA-Z0-9 _-]+)$/);
+
+  const validEmailRegex = RegExp(
+    /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+  );
 
   const proceed = (event) => {
     event.preventDefault();
-    props.nextStep(1, data);
+    props.nextStep(1, state);
   };
 
   const useStyles = makeStyles((theme) => ({
@@ -59,127 +111,127 @@ const Signup = (props) => {
   const classes = useStyles();
 
   return (
-    <Container maxWidth='xs' className={classes.paper}>
+    <Container maxWidth="xs" className={classes.paper}>
       <Avatar className={classes.avatar}>
         <PersonIcon className={classes.accountCircle} />
       </Avatar>
-      <Typography variant='h4'>
+      <Typography variant="h4">
         Not a Member Yet?
         <br /> Sign Up!
       </Typography>
       <form className={classes.form} noValidate onSubmit={props.handleSubmit}>
         <TextField
-          variant='outlined'
-          margin='normal'
+          variant="outlined"
+          margin="normal"
           required
           fullWidth
-          id='name'
-          label='Name'
-          name='name'
-          autoComplete='name'
+          id="name"
+          label="Name"
+          name="name"
+          autoComplete="name"
           autoFocus
           value={props.name}
-          onChange={(e) => (data.name = e.target.value)}
+          onChange={(e) => handleChange(e.target)}
         />
         <TextField
-          variant='outlined'
-          margin='normal'
+          variant="outlined"
+          margin="normal"
           required
           fullWidth
-          id='username'
-          label='Username'
-          name='username'
-          autoComplete='username'
+          id="username"
+          label="Username"
+          name="username"
+          autoComplete="username"
           autoFocus
           value={props.username}
-          onChange={(e) => (data.username = e.target.value)}
+          onChange={(e) => handleChange(e.target)}
         />
         <TextField
-          variant='outlined'
-          margin='normal'
+          variant="outlined"
+          margin="normal"
           required
           fullWidth
-          id='email'
-          label='Email Address'
-          name='email'
-          autoComplete='email'
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
           autoFocus
           value={props.email}
-          onChange={(e) => (data.email = e.target.value)}
+          onChange={(e) => handleChange(e.target)}
         />
         {props.errors ? (
           <TextField
             error
-            variant='outlined'
-            margin='normal'
+            variant="outlined"
+            margin="normal"
             required
             fullWidth
-            name='password'
-            label='Password'
-            type='password'
-            id='password'
-            autoComplete='current-password'
-            onChange={(e) => (data.password = e.target.value)}
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={(e) => handleChange(e.target)}
           />
         ) : (
           <TextField
-            variant='outlined'
-            margin='normal'
+            variant="outlined"
+            margin="normal"
             required
             fullWidth
-            name='password'
-            label='Password'
-            type='password'
-            id='password'
-            autoComplete='current-password'
-            onChange={(e) => (data.password = e.target.value)}
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={(e) => handleChange(e.target)}
           />
         )}
         {props.errors ? (
           <TextField
             error
-            variant='outlined'
-            margin='normal'
+            variant="outlined"
+            margin="normal"
             required
             fullWidth
-            name='password_confirmation'
-            label='Confirm Password'
-            type='password'
-            id='password'
-            autoComplete='current-password'
-            onChange={(e) => (data.password_confirmation = e.target.value)}
+            name="password_confirmation"
+            label="Confirm Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={(e) => handleChange(e.target)}
           />
         ) : (
           <TextField
-            variant='outlined'
-            margin='normal'
+            variant="outlined"
+            margin="normal"
             required
             fullWidth
-            name='password_confirmation'
-            label='Confirm Password'
-            type='password'
-            id='password-confirmation'
-            autoComplete='current-password'
-            onChange={(e) => (data.password_confirmation = e.target.value)}
+            name="password_confirmation"
+            label="Confirm Password"
+            type="password"
+            id="password-confirmation"
+            autoComplete="current-password"
+            onChange={(e) => handleChange(e.target)}
           />
         )}
 
         <TextField
-          variant='outlined'
-          margin='normal'
+          variant="outlined"
+          margin="normal"
           required
           fullWidth
-          name='postal_code'
-          label='Postal Code'
-          type='postal'
-          id='postal-code'
-          autoComplete='postal-code'
-          onChange={(e) => (data.postal_code = e.target.value)}
+          name="postal_code"
+          label="Postal Code"
+          type="postal"
+          id="postal-code"
+          autoComplete="postal-code"
+          onChange={(e) => handleChange(e.target)}
         />
         <Button
           classNames={classes.button}
-          color='primary'
-          variant='contained'
+          color="primary"
+          variant="contained"
           onClick={proceed}
         >
           Continue
