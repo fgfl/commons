4b8e08b0f628f5dfa1029d274a1c0182c3e39d5f
fgfl <frederick.gfl@gmail.com>
2020-02-22 18:11:52 -0800

    Add storybook dependancy back

--- src/stories/1-profile.stories.js
+++ src/stories/1-profile.stories.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
-import Profile from '../components/profile/Profile';
+import Profile from '../views/ProfilePage/Profile';
 
 const user = {
   name: 'Fred',
