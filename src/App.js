bffd2e130e0e953f6c1a2298dda3976a6cc61cea
Pascal van Leeuwen <commoddity@users.noreply.github.com>
2020-02-23 01:06:51 -0800

    Fixed merge conflicts and bill prop passing.

--- src/App.js
+++ src/App.js
@@ -10,132 +10,132 @@ import SignupPage from 'views/SignupPage/SignupPage.js';
 import WatchListPage from 'views/WatchListPage/WatchListPage.js';
 
 const App = (props) => {
-  const [user, setUser] = useState();
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [bills, setBills] = useState([]);
-  const [categories, setCategories] = useState([]);
-  // const [events, setEvents] = useState([]);
+	const [user, setUser] = useState();
+	const [loggedIn, setLoggedIn] = useState(false);
+	const [bills, setBills] = useState([]);
+	const [categories, setCategories] = useState([]);
+	// const [events, setEvents] = useState([]);
 
-  // Loads initial page state and fetches bills/categories
-  useEffect(() => {
-    loginStatus();
-    fetchBills();
-  }, []);
+	// Loads initial page state and fetches bills/categories
+	useEffect(() => {
+		loginStatus();
+		fetchBills();
+	}, []);
 
-  // Fetches bills from the Rails back end
-  const fetchBills = async () => {
-    try {
-      const response = await axios.get(
-        `${process.env.REACT_APP_PUBLIC_URL}/bills`
-      );
-      console.log(response.data.bills);
-      setBills(response.data.bills);
-      setCategories(response.data.categories);
-    } catch (error) {
-      console.error('Error occured on fetchBills:', error);
-    }
-  };
-  const loginStatus = async () => {
-    try {
-      const response = await axios.get(
-        `${process.env.REACT_APP_PUBLIC_URL}/logged_in`
-      );
-      if (response.data.logged_in) {
-        handleLogin(response.data);
-      } else {
-        handleLogout();
-      }
-    } catch (error) {
-      console.error('Error occured on loginStatus:', error);
-    }
-  };
+	// Fetches bills from the Rails back end
+	const fetchBills = async () => {
+		try {
+			const response = await axios.get(
+				`${process.env.REACT_APP_PUBLIC_URL}/bills`
+			);
+			console.log(response.data.bills);
+			setBills(response.data.bills);
+			setCategories(response.data.categories);
+		} catch (error) {
+			console.error('Error occured on fetchBills:', error);
+		}
+	};
+	const loginStatus = async () => {
+		try {
+			const response = await axios.get(
+				`${process.env.REACT_APP_PUBLIC_URL}/logged_in`
+			);
+			if (response.data.logged_in) {
+				handleLogin(response.data);
+			} else {
+				handleLogout();
+			}
+		} catch (error) {
+			console.error('Error occured on loginStatus:', error);
+		}
+	};
 
-  const handleProfileUpdate = async (user) => {
-    try {
-      axios.put(`${process.env.REACT_APP_PUBLIC_URL}/users/${user.id}`, {
-        user
-      });
-      setUser(user);
-    } catch (error) {
-      console.error(`Failed setting profile: ${error}`);
-    }
-  };
+	const handleProfileUpdate = async (user) => {
+		try {
+			axios.put(`${process.env.REACT_APP_PUBLIC_URL}/users/${user.id}`, {
+				user
+			});
+			setUser(user);
+		} catch (error) {
+			console.error(`Failed setting profile: ${error}`);
+		}
+	};
 
-  // Login/logout handlers
-  const handleLogin = (data) => {
-    setUser(data.user);
-    setLoggedIn(true);
-  };
+	// Login/logout handlers
+	const handleLogin = (data) => {
+		setUser(data.user);
+		setLoggedIn(true);
+	};
 
-  const handleLogout = () => {
-    setUser({});
-    setLoggedIn(false);
-  };
+	const handleLogout = () => {
+		setUser({});
+		setLoggedIn(false);
+	};
 
-  return (
-    <div>
-      <Router history={props.hist}>
-        <Switch>
-          <Route
-            exact
-            path='/'
-            render={(props) => (
-              <Home
-                {...props}
-                bills={bills}
-                categories={categories}
-                handleLogout={handleLogout}
-                loggedInStatus={loggedIn}
-              />
-            )}
-          />
-          <Route
-            path='/login-page'
-            render={(props) => (
-              <LoginPage
-                {...props}
-                handleLogin={handleLogin}
-                loggedInStatus={loggedIn}
-                history={props.history}
-              />
-            )}
-          />
-          <Route
-            path='/signup-page'
-            render={(props) => (
-              <SignupPage
-                {...props}
-                categories={categories}
-                handleLogin={handleLogin}
-                loggedInStatus={loggedIn}
-              />
-            )}
-          />
-          <Route
-            path='/watch-list'
-            render={(props) => (
-              <WatchListPage
-                {...props}
-                bills={bills}
-                categories={categories}
-                handleLogin={handleLogin}
-                loggedInStatus={loggedIn}
-              />
-            )}
-          />
-          <Route
-            path='/user/:id'
-            render={() => (
-              <ProfilePage
-                user={user}
-                handleProfileUpdate={handleProfileUpdate}
-              />
-            )}
-          />
-        </Switch>
-      </Router>
-    </div>
-  );
+	return (
+		<div>
+			<Router history={props.hist}>
+				<Switch>
+					<Route
+						exact
+						path='/'
+						render={(props) => (
+							<Home
+								{...props}
+								bills={bills}
+								categories={categories}
+								handleLogout={handleLogout}
+								loggedInStatus={loggedIn}
+							/>
+						)}
+					/>
+					<Route
+						path='/login-page'
+						render={(props) => (
+							<LoginPage
+								{...props}
+								handleLogin={handleLogin}
+								loggedInStatus={loggedIn}
+								history={props.history}
+							/>
+						)}
+					/>
+					<Route
+						path='/signup-page'
+						render={(props) => (
+							<SignupPage
+								{...props}
+								categories={categories}
+								handleLogin={handleLogin}
+								loggedInStatus={loggedIn}
+							/>
+						)}
+					/>
+					<Route
+						path='/watch-list'
+						render={(props) => (
+							<WatchListPage
+								{...props}
+								bills={bills}
+								categories={categories}
+								handleLogin={handleLogin}
+								loggedInStatus={loggedIn}
+							/>
+						)}
+					/>
+					<Route
+						path='/user/:id'
+						render={() => (
+							<ProfilePage
+								user={user}
+								handleProfileUpdate={handleProfileUpdate}
+							/>
+						)}
+					/>
+				</Switch>
+			</Router>
+		</div>
+	);
 };
 
 export default App;
