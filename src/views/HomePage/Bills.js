83c49ee3fa8b24c6e7b25bf94c117400c3dd67e7
Mitch Lum <mitchlum@Mitchs-MacBook-Pro.local>
2020-02-22 18:25:16 -0800

    added billCard and bills to set clicked state for watch button

---
+++ src/views/HomePage/Bills.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
+import BillCard from './BillCard';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    textAlign: 'left',
+    marginBottom: '16px'
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest
+    })
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)'
+  },
+  avatar: {
+    backgroundColor: red[500],
+    width: '60px',
+    height: '60px',
+    fontWeight: 900
+  }
+}));
+
+export default function Bill(props) {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  const billCards = props.bills.map((bill) => {
+    let color = props.clicked[bill.id] ? 'red' : 'grey';
+    return (
+      <BillCard
+        key={bill.id}
+        setThisOneClicked={props.setThisOneClicked}
+        key={props.bill.id}
+        bill={props.bill}
+        style={{ color: color }}
+      />
+    );
+  });
+
+  return billCards;
+}
