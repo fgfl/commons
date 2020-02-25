b087dc49dd8b88e2ea876af1c6da990e8552c2b8
fgfl <frederick.gfl@gmail.com>
2020-02-25 12:36:18 -0800

    Add loading spinner animation while bills load
    - Add loading state
    - show loading spinner while waiting for bills to be fetched

--- src/stories/1-profile.stories.js
+++ src/stories/1-profile.stories.js
@@ -3,17 +3,16 @@ import React, { Fragment } from 'react';
 import { storiesOf } from '@storybook/react';
 
 import Profile from '../views/ProfilePage/Profile';
-import FindMyMp from '../components/FindMyMp/FindMyMp';
 
 const user = {
-	name: 'Fred',
-	username: 'Ffff',
-	email: 'fff@fff.com',
-	password_digest: 'hello',
-	phone_number: '123-456-7890',
-	postal_code: 'A1B 2C3',
-	email_notification: true,
-	sms_notification: true
+  name: 'Fred',
+  username: 'Ffff',
+  email: 'fff@fff.com',
+  password_digest: 'hello',
+  phone_number: '123-456-7890',
+  postal_code: 'A1B 2C3',
+  email_notification: true,
+  sms_notification: true,
 };
 
 storiesOf('Profile', module)
