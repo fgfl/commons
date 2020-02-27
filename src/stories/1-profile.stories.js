b1259677c3b0e560ec868add6f7141714f6fa324
fgfl <frederick.gfl@gmail.com>
2020-02-26 16:49:14 -0800

    update loadingSPinner to work within a component.

--- src/stories/1-profile.stories.js
+++ src/stories/1-profile.stories.js
@@ -10,35 +10,61 @@ const user = {
   email: 'fff@fff.com',
   password_digest: 'hello',
   phone_number: '123-456-7890',
-  postal_code: 'A1B 2C3',
+  postal_code: 'A1B2C3',
   email_notification: true,
   sms_notification: true,
+  user_categories: [1, 3, 5, 9],
+  user_bills: [3, 5, 6],
 };
+
+const categories = [
+  'oen',
+  'two',
+  'three',
+  'four',
+  'five',
+  'six',
+  'seven',
+  'eigth',
+  'nine',
+  'ten',
+];
+
+const handleProfileUpdate = () => {};
 
 storiesOf('Profile', module)
   .add('Default', () => {
+    return (
+      <Fragment>
+        <Profile
+          user={user}
+          handleProfileUpdate={handleProfileUpdate}
+          categories={categories}
+        ></Profile>
+      </Fragment>
+    );
+  })
+  .add('No postal code saved', () => {
     const user = {
       name: 'Fred',
       username: 'Ffff',
       email: 'fff@fff.com',
       password_digest: 'hello',
       phone_number: '123-456-7890',
-      postal_code: 'A1B 2C3',
+      postal_code: '',
       email_notification: true,
       sms_notification: true,
+      user_categories: [1, 3, 5, 9],
+      user_bills: [3, 5, 6],
     };
-    return (
-      <Fragment>
-        <Profile user={user}></Profile>
-      </Fragment>
-    );
-  })
-  .add('No postal code saved', () => {
-    user.postal_code = '';
 
     return (
       <Fragment>
-        <Profile user={user}></Profile>
+        <Profile
+          user={user}
+          handleProfileUpdate={handleProfileUpdate}
+          categories={categories}
+        ></Profile>
       </Fragment>
     );
   });
--- src/stories/3-loadingSpinner.stories.js
+++ src/stories/3-loadingSpinner.stories.js
@@ -5,12 +5,29 @@ import { storiesOf } from '@storybook/react';
 import LoadingSpinner from '../views/LoadingSpinner/LoadingSpinner';
 
 storiesOf('LoadingSpinner', module)
-  .add('Default', () => <LoadingSpinner></LoadingSpinner>)
+  .add('Centered within page', () => (
+    <div
+      style={{
+        minHeight: '100vh',
+        minWidth: '100vw',
+        backgroundColor: 'yellow',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+      }}
+    >
+      <LoadingSpinner></LoadingSpinner>
+    </div>
+  ))
   .add('Centered within element', () => (
     <div
-      style="width: 200px;
-	height: 200px;
-	background-color: red;"
+      style={{
+        position: 'absolute',
+        left: '50px',
+        width: '200px',
+        height: '200px',
+        'background-color': 'red',
+      }}
     >
       <LoadingSpinner></LoadingSpinner>
     </div>
