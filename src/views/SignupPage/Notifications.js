0c3019c4f6ad25df9d032bc8ffa4b32c395c191c
fgfl <frederick.gfl@gmail.com>
2020-03-04 00:53:36 -0800

    Pass saved values back to Notification form.
    - Always save new values regardless if blank. That lets us reset to
    blank value if user changes mind.

--- src/views/SignupPage/Notifications.js
+++ src/views/SignupPage/Notifications.js
@@ -12,14 +12,16 @@ import Avatar from '@material-ui/core/Avatar';
 import Container from '@material-ui/core/Container';
 import AnnouncementIcon from '@material-ui/icons/Announcement';
 
+import validationFunctions from '../../helpers/validationFunctions';
+
 const Notifications = (props) => {
   const [state, setState] = useState({
-    smsNotification: false,
-    emailNotification: false,
-    phoneNumber: '',
+    smsNotification: props.smsNotification,
+    emailNotification: props.emailNotification,
+    phoneNumber: props.phoneNumber,
     errors: {
-      phoneNumber: ''
-    }
+      phoneNumber: '',
+    },
   });
   const [submitted, setSubmitted] = useState(false);
 
@@ -36,7 +38,7 @@ const Notifications = (props) => {
     setState((prevState) => ({
       ...prevState,
       phoneNumber: parsedValue,
-      errors
+      errors,
     }));
   };
 
@@ -62,14 +64,18 @@ const Notifications = (props) => {
   };
 
   const handleEmailCheck = () => {
-    if (state.smsNotification === false) {
-      state.smsNotification = true;
-    }
+    setState((prev) => ({
+      ...prev,
+      emailNotification: !prev.emailNotification,
+      errors: { ...prev.errors },
+    }));
   };
   const handleSmsCheck = () => {
-    if (state.emailNotification === false) {
-      state.emailNotification = true;
-    }
+    setState((prev) => ({
+      ...prev,
+      smsNotification: !prev.smsNotification,
+      errors: { ...prev.errors },
+    }));
   };
 
   const useStyles = makeStyles((theme) => ({
@@ -80,7 +86,7 @@ const Notifications = (props) => {
       alignItems: 'center',
       border: 5,
       padding: theme.spacing(2),
-      textAlign: 'center'
+      textAlign: 'center',
     },
     avatar: {
       zIndex: 1000,
@@ -88,61 +94,61 @@ const Notifications = (props) => {
       marginBottom: theme.spacing(2),
       width: '120px',
       height: '120px',
-      backgroundColor: '#29c0a8'
+      backgroundColor: '#29c0a8',
     },
     formControl: {
       zIndex: 1000,
       width: '100%', // Fix IE 11 issue.
       marginTop: theme.spacing(1),
-      textAlign: 'left'
+      textAlign: 'left',
     },
     submit: {
       margin: theme.spacing(3, 0, 2),
-      backgroundColor: '#29c0a8'
+      backgroundColor: '#29c0a8',
     },
     accountCircle: {
       width: '90px',
       height: '90px',
-      color: 'white'
+      color: 'white',
     },
     error: {
-      color: 'red'
-    }
+      color: 'red',
+    },
   }));
   const classes = useStyles();
 
   return (
     <div>
-      <Container maxWidth='xs' className={classes.paper}>
+      <Container maxWidth="xs" className={classes.paper}>
         <Avatar className={classes.avatar}>
           <AnnouncementIcon className={classes.accountCircle} />
         </Avatar>
-        <Typography variant='h4'>Get updates!</Typography>
-        <FormControl component='fieldset' className={classes.formControl}>
-          <FormLabel component='legend'>
+        <Typography variant="h4">Get updates!</Typography>
+        <FormControl component="fieldset" className={classes.formControl}>
+          <FormLabel component="legend">
             How would you like to receive notifications?
           </FormLabel>
           <FormGroup>
             <FormControlLabel
-              control={<Checkbox />}
-              label='Receive notifications by email'
+              control={<Checkbox checked={state.emailNotification} />}
+              label="Receive notifications by email"
               onChange={handleEmailCheck}
             />
             <FormControlLabel
-              control={<Checkbox />}
-              label='Receive notifications by SMS message'
+              control={<Checkbox checked={state.smsNotification} />}
+              label="Receive notifications by SMS message"
               onChange={handleSmsCheck}
             />
             <TextField
-              variant='outlined'
-              margin='normal'
+              variant="outlined"
+              margin="normal"
               fullWidth
-              id='phoneNumber'
-              label='Phone Number'
-              name='phoneNumber'
-              autoComplete='phoneNumber'
+              id="phoneNumber"
+              label="Phone Number"
+              name="phoneNumber"
+              autoComplete="phoneNumber"
               autoFocus
-              value={props.phoneNumber}
+              defaultValue={state.phoneNumber}
               onChange={(e) => handleChange(e.target)}
             />
             {submitted && errors.phoneNumber.length > 0 && (
@@ -152,8 +158,8 @@ const Notifications = (props) => {
         </FormControl>
         <Button
           className={classes.button}
-          color='secondary'
-          variant='contained'
+          color="secondary"
+          variant="contained"
           onClick={back}
         >
           Back
@@ -161,8 +167,8 @@ const Notifications = (props) => {
 
         <Button
           className={classes.button}
-          color='primary'
-          variant='contained'
+          color="primary"
+          variant="contained"
           onClick={proceed}
         >
           Continue
